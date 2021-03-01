c1b45162fda51a62823a69c1a8be322e49dca62c
Cross <30955151+chaoszeroomega@users.noreply.github.com>
2021-03-01 21:11:09 +1100

    Added VFX  and music
    Final additional feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,10 +19,17 @@ public class PlayerController : MonoBehaviour
     public int hospicount;
     private int soldiermax;
     private float timeresult;
+    public AudioSource music;
+    public AudioClip boom;
+    public AudioClip pickup;
+    public AudioClip dropoff;
+
+
     // Start is called before the first frame update
     void Start()
     {
         soldiermax = 4;
+        music.Play();
     }
 
     // Update is called once per frame
@@ -51,21 +58,26 @@ public class PlayerController : MonoBehaviour
         {
             if (soldiercount < 3)
             {
+               
                 collision.gameObject.SetActive(false);
             
                 soldiercount++;
+                music.PlayOneShot(pickup, 1f);
             }
 
         }
         if(collision.gameObject.tag == "Tree")
         {
+            music.Stop();
             this.gameObject.SetActive(false);
             gameOverText.gameObject.SetActive(true);
+            music.PlayOneShot(boom, 1f);
         }
-        if (collision.gameObject.tag == "Hospital")
+        if (collision.gameObject.tag == "Hospital" && soldiercount > 0)
         {
             hospicount += soldiercount;
             soldiercount = 0;
+            music.PlayOneShot(dropoff, 1f);
 
         }
     }
